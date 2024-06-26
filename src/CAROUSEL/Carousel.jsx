acede9a99eaf906bc9b0fb0bd8cbfac2a794227c
Abdullah Shahed <142104079+shahed32156@users.noreply.github.com>
2024-06-26 14:38:26 +0600

    Update Carousel.jsx

--- src/CAROUSEL/Carousel.jsx
+++ src/CAROUSEL/Carousel.jsx
@@ -11,7 +11,7 @@ const Carousel = () => {
         
           <h1 className='text-center text-2xl font-mono font-semibold md:text-4xl'>Enjoy your travel with safe journey!</h1>
         
-        <img className='mt-10 md:mt-16 h-[150px] w-full md:h-[400px] md:w-[900px] rounded-[10rem] mx-auto' src="./Images/cloud.png" alt="" />
+        <img className='mt-10 md:mt-16 h-[150px] w-full md:h-[400px] md:w-[900px] rounded-[10rem] mx-auto' src="./Images/cloud.gif" alt="" />
         <img className='h-[100px] w-[250px] md:h-[250px] md:w-[600px] rounded-[10rem] absolute top-20 lg:top-24 left-0 lg:left-[280px]' src="./Images/plane.png" alt="" />
      </div>
     
